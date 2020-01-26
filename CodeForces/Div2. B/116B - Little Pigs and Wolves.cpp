f91cd8aa861fb7c7cc9f0fe7ff96c29801463c4a
Omar Shatta <omarshatta1998@gmail.com>
2020-01-26 11:11:10 +0200

    Update 116B - Little Pigs and Wolves.cpp

--- CodeForces/Div2. B/116B - Little Pigs and Wolves.cpp
+++ CodeForces/Div2. B/116B - Little Pigs and Wolves.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+ 
 
+int main()
+{
+    int n,m,res=0;
+    cin>>n>>m;
+    char arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];}}
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if (arr[i][j]=='W'){
+                if ((arr[i-1][j]=='P'&&i!=0)||(arr[i][j-1]=='P'&&j!=0)||(arr[i+1][j]=='P'&&(i!=n-1))||(arr[i][j+1]=='P'&&(j!=m-1))){
+                    res+=1;
+                }
+            }
+        }
+    }
+    cout<<res;
+    return 0;
+}
